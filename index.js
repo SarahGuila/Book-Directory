512533932d8a1bedf48975f3a30c373661f1bd97
Sarah Guila <sarahguila26@gmail.com>
2022-11-17 00:21:38 +0200

    Add BL

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const express = require('express')
 const {Book} = require('./model')
+var mongoose = require('mongoose');
 const { connectToMongo } = require('./dbconfig')
-const { appendFile } = require('fs/promises')
 
+
+connectToMongo()
 
 const app = express()
 app.use(express.json())
 
 app.get('/api/books', async (req, res) => {
     try {
-        res.find({bookName})
+        res.send(await Book.find())
     }catch (error) {
-        res.status(500).send({ error })
+        res.status(500).send({ error: "There is no books availble" })
     }
 })
 
 app.post('/api/newBook', async (req,res) => {
     try {
-        const newBook = new Book(req.body)
-        const book =  await newBook.Save()
-        res.send('Book added successfully')
-    }catch (error) {
-        res.status(500).send({ message: error })
+        const {bookName,hauther} = (req.body)
+        const newBook = new Book({bookName,hauther})
+        //mongoose.model('book').findOne({bookName}),async function (err, result) {
+        const exist = await Book.findOne({bookName,hauther})
+        if (!exist) {
+            await newBook.save()
+            res.send({msg:'Book added successfully'})
+        }else{
+            res.send({msg:'Book Exist'})
+        }
+   }catch (error) {
+        res.status(500).send({ message: "cannot save book" })
     }
 })
-
 app.listen(8080, _ => console.log('server is up and runing on port 8080'))
